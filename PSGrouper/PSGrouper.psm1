3006d9199189c6aa8fa1b5044181cafaff3d00f1
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2021-02-25 16:00:48 +0100

    Moved PowerShell module to it's own repository

--- PSGrouper/PSGrouper.psm1
+++
@@ -1,7 +0,0 @@
-﻿# Hide progress bar for Invoke-WebRequest
-$ProgressPreference = 'SilentlyContinue'
-
-$functions = Get-ChildItem -Path "$PSScriptRoot\Functions\*.ps1" | Select-Object -ExpandProperty FullName
-foreach ($func in $functions) {
-    . $func
-}
