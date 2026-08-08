1e17af276a0ec11e60a52622e6278db4a48a2465
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2026-08-09 00:11:20 +0200

    IEquals now compares null correctly

--- GrouperLib/GrouperLib.Core/StringExtensions.cs
+++ GrouperLib/GrouperLib.Core/StringExtensions.cs
@@ -2,9 +2,6 @@
 
 static class StringExtensions
 {
-    public static bool IEquals(this string? value, string? other)
-    {
-        // This deviates from standard string.Equals which will throw if value is null
-        return value != null && value.Equals(other, StringComparison.OrdinalIgnoreCase);
-    }
+    public static bool IEquals(this string? value, string? other) =>
+        string.Equals(value, other, StringComparison.OrdinalIgnoreCase);
 }
